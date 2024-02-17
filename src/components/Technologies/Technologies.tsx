78bd84028338f5db7394c5aeb1166eccf82b7364
Ricardo Gava <ricardoagava@gmail.com>
2024-02-17 03:11:21 -0300

    feat: add Technologies component

---
+++ src/components/Technologies/Technologies.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image"
+import hexagonGreen from "/public/hexagonGreen.svg"
+import hexagonOrange from "/public/hexagonOrange.svg"
+
+type TechnologiesProps = {
+  techs: string[],
+  libs: string[]
+}
+
+export const Technologies = ({ techs, libs }: TechnologiesProps) => {
+  return (
+    <div className="text-white uppercase font-medium text-lg">
+      {techs.length > 0 &&
+        <div className="flex relative gap-2">
+          {techs.map(tech =>
+            <div key={tech} className="flex relative justify-center items-center">
+              <p className="absolute text-center">{tech}</p>
+              <Image key={tech} src={hexagonGreen} width={120} height={120} alt="" />
+            </div>
+          )}
+        </div>
+      }
+
+      {libs.length > 0 &&
+        <div className="flex gap-2 relative left-[64px] -top-[26px]">
+          {libs.map(lib =>
+            <div key={lib} className="flex relative justify-center items-center">
+              <p className="absolute px-2 text-center">{lib}</p>
+              <Image src={hexagonOrange} width={120} height={120} alt="" />
+            </div>
+          )}
+        </div>
+      }
+    </div>
+  )
+}
