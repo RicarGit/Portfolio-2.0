683650fb0828fcb9d5469222cb54e1da34bbcc86
Ricardo Gava <ricardoagava@gmail.com>
2024-02-24 23:24:34 -0300

    refactor: refactoring some styles in aside, footer and p html tags

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -4,20 +4,20 @@ import { SubTitle } from "../SubTitle/SubTitle"
 
 export const SideMenu = () => {
   return (
-    <aside className="fixed left-0 z-30 flex flex-col h-screen bg-dark-green border-r-[3px] border-y-[3px] rounded-r bg-[radial-gradient(circle_at_0_25%,_var(--tw-gradient-stops))] from-green via-dark-green via-70% to-dark-green">
+    <aside className="absolute z-30 pt-5 flex flex-col h-screen bg-dark-green border-r-[3px] border-y-[3px] rounded-r bg-[radial-gradient(circle_at_0_25%,_var(--tw-gradient-stops))] from-green via-dark-green via-70% to-dark-green">
       <SubTitle>Projetos</SubTitle>
 
       <nav className="text-white">
         <ProjectsList />
       </nav>
 
-      <footer className="h-full">
+      <footer className="h-full relative">
         <SubTitle>Contato</SubTitle>
 
         <Contact contact="ricardoagava@gmail.com">Email:</Contact>
         <Contact contact="(11) 97328-3296">Whatsapp:</Contact>
 
-        <p className="text-dark-blue text-xs tracking-wide text-center relative bottom-[-20%]">Todos os direitos reservados.</p>
+        <p className="text-dark-blue text-xs tracking-wide absolute bottom-4 ml-4">Todos os direitos reservados.</p>
       </footer>
 
     </aside>
