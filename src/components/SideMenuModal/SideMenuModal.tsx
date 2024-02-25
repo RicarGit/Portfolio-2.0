1dee7654a568bc1bd8d6600aee076a635d74eb94
Ricardo Gava <ricardoagava@gmail.com>
2024-02-24 23:35:04 -0300

    refactor: refactored some sizes and space styles

--- src/components/SideMenuModal/SideMenuModal.tsx
+++ src/components/SideMenuModal/SideMenuModal.tsx
@@ -7,11 +7,10 @@ import { Technologies } from "../Technologies/Technologies"
 import { ProjectDiamond } from "../ProjectDiamond/ProjectDiamond"
 import { ModalDetail } from "../ModalDetail/ModalDetail"
 
-
 export const SideMenuModal = () => {
   return (
     <div className="absolute w-screen h-screen flex items-center z-20 backdrop-blur-sm">
-      <div className="absolute z-30 w-[70%] min-h-[80%] left-[276px] grid grid-cols-2 grid-rows-2 px-8 border-3 rounded bg-[radial-gradient(circle_at_80%_20%,_var(--tw-gradient-stops))] from-green via-dark-green via-50% to-dark-green">
+      <div className="absolute z-30 w-[70%] min-h-[80%] left-[300px] grid grid-cols-2 grid-rows-2 px-5 border-3 rounded bg-[radial-gradient(circle_at_80%_20%,_var(--tw-gradient-stops))] from-green via-dark-green via-50% to-dark-green">
 
         <ModalDetail color="green" position="top-right" />
         <ModalDetail color="blue" position="bottom-right" />
@@ -19,7 +18,7 @@ export const SideMenuModal = () => {
         <div className="relative">
           <SubTitle>Wildbeast</SubTitle>
 
-          <p className="text-light-green max-w-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at augue non tellus pharetra laoreet. Sed venenatis aliquet scelerisque. Integer aliquet cursus est, ac placerat neque tempor eleifend. Nulla non facilisis neque, at venenatis turpis, Nulla non facilisis neque, at venenatis turpis.
+          <p className="text-light-green max-w-[600px] pl-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at augue non tellus pharetra laoreet. Sed venenatis aliquet scelerisque. Integer aliquet cursus est, ac placerat neque tempor eleifend. Nulla non facilisis neque, at venenatis turpis, Nulla non facilisis neque, at venenatis turpis.
           </p>
 
           <div className="flex gap-12 mt-14 ml-5">
@@ -30,12 +29,11 @@ export const SideMenuModal = () => {
           <Image src={separator} width={500} height={6} alt="" className="absolute bottom-[-6px]" />
         </div>
 
-
         <div className="row-span-2 flex justify-center items-center">
           <ProjectDiamond />
         </div>
 
-        <div>
+        <div className="mt-2">
           <SubTitle>Principais Tecnologias</SubTitle>
           <Technologies techs={['html', 'css', 'ts', 'react']} libs={['axios', 'zustand', 'router dom', 'hook form']} />
         </div>
