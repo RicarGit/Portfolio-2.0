64cdd897e9a4ba6d4e517ee5164a46976f905ca4
Ricardo Gava <ricardoagava@gmail.com>
2024-02-26 23:59:37 -0300

    feat: add projectsData

---
+++ src/data/projectsData.ts
@@ -0,0 +1,64 @@
+import dogsSocialMediaThumb from "/public/dogsSocialMediaThumb.jpg"
+import wildbeastThumb from "/public/wildbeastThumb.jpg"
+import galeriaFotosThumb from "/public/galeriaFotosThumb.jpg"
+import devMemoryThumb from "/public/devMemoryThumb.jpg"
+import calculadoraIMCThumb from "/public/calculadoraIMCThumb.jpg"
+import albumFotosThumb from "/public/albumFotosThumb.jpg"
+
+import dogsSocialMedia from "/public/dogsSocialMedia.png"
+import wildbeast from "/public/wildbeast.png"
+import galeriaFotos from "/public/galeriaFotos.png"
+import devMemory from "/public/devMemory.png"
+import calculadoraIMC from "/public/calculadoraIMC.png"
+import albumFotos from "/public/albumFotos.png"
+
+export const projetsData = [
+  {
+    projectName: 'Dogs Social Media',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at augue non tellus pharetra laoreet. Sed venenatis aliquet scelerisque. Integer aliquet cursus est, ac placerat neque tempor eleifend. Nulla non facilisis neque, at venenatis turpis, Nulla non facilisis neque, at venenatis turpis.',
+    techs: ['html', 'css', 'react'],
+    libs: [''],
+    thumbImage: dogsSocialMediaThumb,
+    projectImage: dogsSocialMedia
+  },
+  {
+    projectName: 'Wildbeast',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at augue non tellus pharetra laoreet. Sed venenatis aliquet scelerisque. Integer aliquet cursus est, ac placerat neque tempor eleifend. Nulla non facilisis neque, at venenatis turpis, Nulla non facilisis neque, at venenatis turpis.',
+    techs: [''],
+    libs: [''],
+    thumbImage: wildbeastThumb,
+    projectImage: wildbeast
+  },
+  {
+    projectName: 'Galeria de Fotos',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at augue non tellus pharetra laoreet. Sed venenatis aliquet scelerisque. Integer aliquet cursus est, ac placerat neque tempor eleifend. Nulla non facilisis neque, at venenatis turpis, Nulla non facilisis neque, at venenatis turpis.',
+    techs: [''],
+    libs: [''],
+    thumbImage: galeriaFotosThumb,
+    projectImage: galeriaFotos
+  },
+  {
+    projectName: 'DevMemory',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at augue non tellus pharetra laoreet. Sed venenatis aliquet scelerisque. Integer aliquet cursus est, ac placerat neque tempor eleifend. Nulla non facilisis neque, at venenatis turpis, Nulla non facilisis neque, at venenatis turpis.',
+    techs: [''],
+    libs: [''],
+    thumbImage: devMemoryThumb,
+    projectImage: devMemory
+  },
+  {
+    projectName: 'Calculadora de IMC',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at augue non tellus pharetra laoreet. Sed venenatis aliquet scelerisque. Integer aliquet cursus est, ac placerat neque tempor eleifend. Nulla non facilisis neque, at venenatis turpis, Nulla non facilisis neque, at venenatis turpis.',
+    techs: [''],
+    libs: [''],
+    thumbImage: calculadoraIMCThumb,
+    projectImage: calculadoraIMC
+  },
+  {
+    projectName: 'Álbum de Fotos',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at augue non tellus pharetra laoreet. Sed venenatis aliquet scelerisque. Integer aliquet cursus est, ac placerat neque tempor eleifend. Nulla non facilisis neque, at venenatis turpis, Nulla non facilisis neque, at venenatis turpis.',
+    techs: [''],
+    libs: [''],
+    thumbImage: albumFotosThumb,
+    projectImage: albumFotos
+  },
+]
