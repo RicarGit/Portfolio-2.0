548af854238e9153d285a3ea7ee66bfae6abb67e
Ricardo Gava <ricardoagava@gmail.com>
2024-03-01 01:25:37 -0300

    refactor: ProjectDiamond component now receive the image via props

--- src/components/ProjectDiamond/ProjectDiamond.tsx
+++ src/components/ProjectDiamond/ProjectDiamond.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image"
-import wildbeast from "/public/wildbeast.png"
+import { StaticImageData } from "next/image"
 
-export const ProjectDiamond = () => {
+type ProjectDiamondProps = {
+  projectImage: StaticImageData
+}
+
+export const ProjectDiamond = ({ projectImage }: ProjectDiamondProps) => {
   return (
-    <div className=" max-w-[400px] min-w-60 border-5 rounded-md border-orange bg-gradient-br-orange rotate-45 relative right-[100px] big-diamond-shadows">
-      <Image src={wildbeast} width={500} height={500} alt="" className={`relative right-3 bottom-[31px] desktop:scale-[1.19] laptop:scale-[1.15] -rotate-45 `} />
+    <div className="max-w-[400px] min-w-60 border-5 rounded-md border-orange bg-gradient-br-orange rotate-45 relative right-[100px] big-diamond-shadows">
+      <Image src={projectImage} width={500} height={500} alt="" className={`relative right-3 bottom-[31px] desktop:scale-[1.19] laptop:scale-[1.15] -rotate-45`} />
     </div>
   )
 }
