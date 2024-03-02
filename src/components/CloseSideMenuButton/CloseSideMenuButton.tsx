55b5c5880a9f73c038ad859c4bdc47263cedb544
Ricardo Gava <ricardoagava@gmail.com>
2024-03-02 12:08:03 -0300

    feat: add CloseSideMenuButton component

---
+++ src/components/CloseSideMenuButton/CloseSideMenuButton.tsx
@@ -0,0 +1,17 @@
+import { SetStateAction } from "react"
+
+type CloseSideMenuButtonProps = {
+  setIsOpen: React.Dispatch<SetStateAction<boolean>>
+}
+
+export const CloseSideMenuButton = ({ setIsOpen }: CloseSideMenuButtonProps) => {
+  return (
+    <button onClick={() => setIsOpen(state => !state)} className="absolute -right-[30px] top-3 max-w-2 pr-4 pl-2 py-2 leading-none bg-dark-green text-white font-medium text-sm rounded-r-md border-y-[3px] border-r-[3px] cursor-pointer overflow-hidden">
+      C
+      L
+      O
+      S
+      E
+    </button>
+  )
+}
