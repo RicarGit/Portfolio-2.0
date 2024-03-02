ea9c9450d22fdde0fa8331af3aa6255a81099836
Ricardo Gava <ricardoagava@gmail.com>
2024-03-02 16:46:36 -0300

    feat: add setIsOpen prop to SideMenu component and pass it to the CloseSideMenuButton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
       {isOpen &&
         <>
-          <SideMenu />
+          <SideMenu setIsOpen={setIsOpen} />
           <SideMenuModal />
         </>
       }
--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,14 +1,23 @@
-import { Contact } from "../Contact/Contact"
-import { ProjectsList } from "../ProjectsList/ProjectsList"
-import { SubTitle } from "../SubTitle/SubTitle"
+import { SetStateAction } from "react"
 
-export const SideMenu = () => {
+import { Contact } from "../Contact/Contact"
+import { ProjectsThumbnailList } from "../ProjectsThumbnailList/ProjectsThumbnailList"
+import { SubTitle } from "../SubTitle/SubTitle"
+import { CloseSideMenuButton } from "../CloseSideMenuButton/CloseSideMenuButton"
+
+type SideMenuProps = {
+  setIsOpen: React.Dispatch<SetStateAction<boolean>>
+}
+
+export const SideMenu = ({ setIsOpen }: SideMenuProps) => {
   return (
     <aside className="absolute z-30 pt-5 flex flex-col h-screen bg-dark-green border-r-[3px] border-y-[3px] rounded-r bg-[radial-gradient(circle_at_0_25%,_var(--tw-gradient-stops))] from-green via-dark-green via-70% to-dark-green">
+      <CloseSideMenuButton setIsOpen={setIsOpen} />
+
       <SubTitle>Projetos</SubTitle>
 
       <nav className="text-white">
-        <ProjectsList />
+        <ProjectsThumbnailList />
       </nav>
 
       <footer className="h-full relative">
