34dc2da4f87ae69da95f9cb96887f8cc55eabd5f
Ricardo Gava <ricardoagava@gmail.com>
2024-03-05 04:55:14 -0300

    chore: removing the hover from the project thumbnail

--- src/components/ProjectThumbnail/ProjectThumbnail.tsx
+++ src/components/ProjectThumbnail/ProjectThumbnail.tsx
@@ -30,7 +30,7 @@ export const ProjectThumbnail = ({ thumbImage, projectIndex, children }: Project
 
   return (
     <li onClick={toogleActive} className="relative w-[230px] h-20 flex justify-center items-center border-t-[3px] last:border-b-[3px] border-white box-content">
-      <div className={`absolute z-10 w-[230px] h-20 bg-[rgba(0,0,0,.6)] hover:bg-[rgba(0,0,0,.45)] ${isActive && 'hidden'}`}></div>
+      <div className={`absolute z-10 w-[230px] h-20 bg-[rgba(0,0,0,.6)] ${isActive && 'hidden'}`}></div>
       <div className={`${isActive && "h-10 w-10 absolute border-b-[3px] border-l-[3px] rounded-sm rotate-45 bg-dark-green -right-[91px] cursor-default"}`}></div>
       <h3 className={`text-lg font-medium relative z-20 ${isActive && 'hidden'}`}>{children}</h3>
       <Image src={thumbImage} fill priority alt="" />
