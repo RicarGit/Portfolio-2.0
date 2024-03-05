5bd7545240af8acd389856d573a122c594949fde
Ricardo Gava <ricardoagava@gmail.com>
2024-03-05 04:54:18 -0300

    chore: changing the close button text to brazilian portuguese, also, with 1 more letter i had to take the button more closer to the top

--- src/components/CloseSideMenuButton/CloseSideMenuButton.tsx
+++ src/components/CloseSideMenuButton/CloseSideMenuButton.tsx
@@ -6,12 +6,13 @@ type CloseSideMenuButtonProps = {
 
 export const CloseSideMenuButton = ({ setIsOpen }: CloseSideMenuButtonProps) => {
   return (
-    <button onClick={() => setIsOpen(state => !state)} className="absolute -right-[30px] top-3 max-w-2 pr-4 pl-2 py-2 leading-none bg-dark-green text-white font-medium text-sm rounded-r-md border-y-[3px] border-r-[3px] cursor-pointer overflow-hidden">
-      C
-      L
-      O
-      S
+    <button onClick={() => setIsOpen(state => !state)} className="absolute -right-[30px] top-1 max-w-2 pr-4 pl-2 py-2 leading-none bg-dark-green text-white font-medium text-sm rounded-r-md border-y-[3px] border-r-[3px] cursor-pointer overflow-hidden">
+      F
       E
+      C
+      H
+      A
+      R
     </button>
   )
 }
