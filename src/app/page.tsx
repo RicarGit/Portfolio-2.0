4ab8f3fe2a2a12c7ef33a291c11054384fa326e4
Ricardo Gava <ricardoagava@gmail.com>
2024-03-18 23:57:17 -0300

    feat: add AboutMe component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import { SideMenuModal } from "@/components/SideMenuModal/SideMenuModal"
 import { SideMenu } from "@/components/SideMenu/SideMenu"
 import { PageDetails } from "@/components/PageDetails/PageDetails"
 import { SocialMediaButtons } from "@/components/SocialMediaButtons/SocialMediaButtons"
+import AboutMe from "@/components/AboutMe/AboutMe"
+
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false)
@@ -32,8 +34,15 @@ export default function Home() {
           <div className="relative text-white">
             <p className="desktop:text-xl text-base tracking-widest">Olá!👋 Meu nome é <span className="font-extrabold bg-gradient-to-r from-orange to-transparent rounded py-0.5 pl-2 inline-block w-[36%]">Ricardo.</span></p>
 
-            <h1 className="desktop:text-4xl text-2xl font-bold mt-8">Sou um<br className="mb-3" />
-              <span className="desktop:text-7xl text-5xl tracking-wide font-black text-dark-green text-stroke relative z-10 bg-clip-text text-transparent bg-gradient-to-t from-green to-dark-green via-50% via-dark-green">Desenvolvedor Front-End</span>.</h1>
+            {!showAboutMe &&
+              <h1 className="desktop:text-4xl text-2xl font-bold mt-8">Sou um<br className="mb-3" />
+                <span className="desktop:text-7xl text-5xl tracking-wide font-black text-dark-green text-stroke relative z-10 bg-clip-text text-transparent bg-gradient-to-t from-green to-dark-green via-50% via-dark-green">Desenvolvedor Front-End</span>.
+              </h1>
+            }
+
+            {showAboutMe &&
+              <AboutMe />
+            }
 
             <div className="flex gap-12 mt-24">
               <Button callbackState={setIsOpen} color="green">menu</Button>
---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image"
+import html5 from '/public/html5-original.svg'
+import css from '/public/css3-original.svg'
+import javascript from '/public/javascript-original.svg'
+import typescript from '/public/typescript-original.svg'
+import react from '/public/react-original.svg'
+import next from '/public/nextjs-original.svg'
+import tailwind from '/public/tailwindcss-original.svg'
+import styledComponents from '/public/styled-components.svg'
+
+export default function AboutMe() {
+  return (
+    <>
+      <p className="mt-4 w-[500px] mr-[138px] text-light-green tracking-wider">
+        Especializado em Front-End com foco na criação de interfaces dinâmicas e responsivas, estou comprometido em criar soluções web de alta qualidade, busco constantemente aprimorar a experiência do usuário através de soluções eficientes e modernas. Explore meu portfólio para ver meu trabalho em ação!
+      </p>
+
+      <div>
+        <h3 className='text-lg text-orange mt-8 justify-center'>Conhecimentos: </h3>
+        <div className='flex gap-3 mt-3'>
+          <Image src={html5} width={40} height={40} alt="html5 icon" />
+          <Image src={css} width={40} height={40} alt="css icon" />
+          <Image src={javascript} width={40} height={40} alt="javascript icon" />
+          <Image src={typescript} width={40} height={40} alt="typescript icon" />
+          <Image src={react} width={40} height={40} alt="react icon" />
+          <Image src={next} width={40} height={40} alt="next icon" />
+          <Image src={tailwind} width={40} height={40} alt="tailwind icon" />
+          <Image src={styledComponents} width={40} height={40} alt="styled-components icon" />
+        </div>
+      </div>
+    </>
+  )
+}
