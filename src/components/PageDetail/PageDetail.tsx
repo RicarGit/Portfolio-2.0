3f3d427d07f237704917a119f0056b21ceb858f7
Ricardo Gava <ricardoagava@gmail.com>
2024-03-20 00:47:43 -0300

    feat: using percentage on the details position, also removed the margin and add an image alt description

--- src/components/PageDetail/PageDetail.tsx
+++ src/components/PageDetail/PageDetail.tsx
@@ -14,16 +14,21 @@ const svgColors = {
 }
 
 const svgPositions = {
-  ['top-right']: 'top-0 right-0',
-  ['top-left']: 'top-0 left-0',
-  ['bottom-right']: 'bottom-0 right-0',
-  ['bottom-left']: 'bottom-0 left-0'
+  ['top-right']: 'top-[5%] right-[2.5%]',
+  ['top-left']: 'top-[5%] left-[2.5%]',
+  ['bottom-right']: 'bottom-[5%] right-[2.5%]',
+  ['bottom-left']: 'bottom-[5%] left-[2.5%]'
 }
 
 export const PageDetail = ({ color, rotate, position }: PageDetail) => {
   return (
-    <div className={`absolute m-12 ${color === 'blue' ? 'w-[350px] h-44' : 'w-44 h-[350px]'} ${svgPositions[position]} ${rotate ?? 'rotate-180'}`}>
-      <Image src={svgColors[color]} fill priority alt="" className="drop-shadow-[8px_8px_1px_rgba(0,0,0,.3)]" />
+    <div className={`absolute ${color === 'blue' ? 'w-[350px] h-44' : 'w-44 h-[350px]'} ${svgPositions[position]} ${rotate ?? 'rotate-180'}`}>
+      <Image
+        src={svgColors[color]}
+        fill
+        priority
+        alt="page detail"
+        className="drop-shadow-[8px_8px_1px_rgba(0,0,0,.3)]" />
     </div>
   )
 }
