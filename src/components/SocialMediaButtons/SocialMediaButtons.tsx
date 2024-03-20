fe90c94c836fef6b601e9fa6aa1023cee7a05efc
Ricardo Gava <ricardoagava@gmail.com>
2024-03-20 00:49:45 -0300

    chore: removed unnecessary css class

--- src/components/SocialMediaButtons/SocialMediaButtons.tsx
+++ src/components/SocialMediaButtons/SocialMediaButtons.tsx
@@ -2,7 +2,7 @@ import { SocialMediaButton } from "../SocialMediaButton/SocialMediaButton"
 
 export const SocialMediaButtons = () => {
   return (
-    <div className="absolute top-[10%] right-0 flex gap-12 justify-end">
+    <div className="absolute top-[10%] flex gap-12 justify-end">
       <SocialMediaButton src='/github.svg' link="https://github.com/RicarGit" alt='github link' />
       <SocialMediaButton src='/linkedin.svg' link="https://www.linkedin.com/in/ricardo-gava-dev/" alt='linkedin link' />
       <SocialMediaButton src='/discord.svg' link="https://discord.gg/e5djqg63u3" alt='discord link' />
