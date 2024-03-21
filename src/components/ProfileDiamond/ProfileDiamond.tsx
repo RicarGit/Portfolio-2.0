1c2c26138d93ce24e51b2d0631ce26c41e32b898
Ricardo Gava <ricardoagava@gmail.com>
2024-03-20 22:30:45 -0300

    feat: add image alt description

--- src/components/ProfileDiamond/ProfileDiamond.tsx
+++ src/components/ProfileDiamond/ProfileDiamond.tsx
@@ -4,7 +4,7 @@ import profileImage from "/public/profile.png"
 export const ProfileDiamond = () => {
   return (
     <div className="max-w-96 min-w-60 max-h-96 min-h-60 border-5 rounded-md border-orange bg-gradient-br-orange rotate-45 relative right-16 big-diamond-shadows">
-      <Image src={profileImage} width={500} height={500} alt="" className="relative right-5 bottom-5 desktop:scale-[1.13] laptop:scale-[1.15]" />
+      <Image src={profileImage} width={500} height={500} alt="profile image" className="relative right-5 bottom-5 desktop:scale-[1.13] laptop:scale-[1.15]" />
     </div>
   )
 }
