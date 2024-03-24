4ed9507f098eb7cc37979d0c7c9d140174c687b4
Ricardo Gava <ricardoagava@gmail.com>
2024-03-24 00:11:48 -0300

    feat: add responsiveness to ProfileDiamond component

--- src/components/ProfileDiamond/ProfileDiamond.tsx
+++ src/components/ProfileDiamond/ProfileDiamond.tsx
@@ -3,8 +3,8 @@ import profileImage from "/public/profile.png"
 
 export const ProfileDiamond = () => {
   return (
-    <div className="max-w-96 min-w-60 max-h-96 min-h-60 border-5 rounded-md border-orange bg-gradient-br-orange rotate-45 relative right-16 big-diamond-shadows">
-      <Image src={profileImage} width={500} height={500} alt="profile image" className="relative right-5 bottom-5 desktop:scale-[1.13] laptop:scale-[1.15]" />
+    <div className="hidden laptop:block max-w-96 min-w-52 max-h-96 min-h-52 border-5 rounded-md border-orange bg-gradient-br-orange rotate-45 relative desktop:right-16 right-12 big-diamond-shadows-small desktop:big-diamond-shadows">
+      <Image src={profileImage} width={500} height={500} alt="profile image" className="relative right-5 bottom-5 desktop:scale-[1.13] scale-[1.19]" />
     </div>
   )
 }
