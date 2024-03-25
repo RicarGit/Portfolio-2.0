8b81a19f99358fc7a36230be9cfb5719ab820046
Ricardo Gava <ricardoagava@gmail.com>
2024-03-25 12:25:19 -0300

    feat: add some resposivity to SideMenu and the projects thumbnail to work better on smaller screens

--- src/components/ProjectThumbnail/ProjectThumbnail.tsx
+++ src/components/ProjectThumbnail/ProjectThumbnail.tsx
@@ -5,12 +5,11 @@ import { ReactNode, useEffect, useState } from "react"
 import { useTabQuery } from "@/hook/useTabQuery"
 
 type ProjectThumbnailProps = {
-  children: ReactNode
   projectIndex: string
   thumbImage: StaticImageData
 }
 
-export const ProjectThumbnail = ({ thumbImage, projectIndex, children }: ProjectThumbnailProps) => {
+export const ProjectThumbnail = ({ thumbImage, projectIndex }: ProjectThumbnailProps) => {
   const [isActive, setIsActive] = useState(false)
   const { setTab, tab, removeTab } = useTabQuery()
 
@@ -29,11 +28,10 @@ export const ProjectThumbnail = ({ thumbImage, projectIndex, children }: Project
   }, [tab, projectIndex])
 
   return (
-    <li onClick={toogleActive} className="relative w-[230px] h-20 flex justify-center items-center border-t-[3px] last:border-b-[3px] border-white box-content">
-      <div className={`absolute z-10 w-[230px] h-20 bg-[rgba(0,0,0,.6)] ${isActive && 'hidden'}`}></div>
-      <div className={`${isActive && "h-10 w-10 absolute border-b-[3px] border-l-[3px] rounded-sm rotate-45 bg-dark-green -right-[91px] cursor-default"}`}></div>
-      <h3 className={`text-lg font-medium relative z-20 ${isActive && 'hidden'}`}>{children}</h3>
-      <Image src={thumbImage} fill priority alt="" />
+    <li onClick={toogleActive} className="relative w-full modal:h-20 h-16 flex justify-center items-center border-t-[3px] last:border-b-[3px] border-white box-content">
+      <div className={`absolute z-10 w-full modal:h-20 h-16 bg-[rgba(0,0,0,.6)] ${isActive && 'hidden'}`}></div>
+      <div className={`${isActive && "h-10 w-10 absolute border-b-[3px] border-l-[3px] rounded-sm rotate-45 bg-dark-green -right-[64px] cursor-default"}`}></div>
+      <Image src={thumbImage} fill priority alt="project thumbnail" />
     </li>
   )
 }
--- src/components/ProjectsThumbnailList/ProjectsThumbnailList.tsx
+++ src/components/ProjectsThumbnailList/ProjectsThumbnailList.tsx
@@ -3,12 +3,10 @@ import { projectsData } from "@/data/projectsData"
 
 export const ProjectsThumbnailList = () => {
   return (
-    <ul className="cursor-pointer">
+    <ul className="cursor-pointer w-full">
       {projectsData &&
         projectsData.map(({ projectName, thumbImage }, index) =>
-          <ProjectThumbnail key={projectName} projectIndex={index.toString()} thumbImage={thumbImage}>
-            {projectName}
-          </ProjectThumbnail>
+          <ProjectThumbnail key={projectName} projectIndex={index.toString()} thumbImage={thumbImage} />
         )}
     </ul>
   )
