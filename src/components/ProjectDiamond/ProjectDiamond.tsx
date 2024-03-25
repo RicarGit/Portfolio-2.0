a2f335e258ad2c2cb449df5b12fc4f534f9ccaa0
Ricardo Gava <ricardoagava@gmail.com>
2024-03-25 12:28:37 -0300

    style: changing the max size width, shadow sizes and the image scale to work better on smaller screens

--- src/components/ProjectDiamond/ProjectDiamond.tsx
+++ src/components/ProjectDiamond/ProjectDiamond.tsx
@@ -7,8 +7,8 @@ type ProjectDiamondProps = {
 
 export const ProjectDiamond = ({ projectImage }: ProjectDiamondProps) => {
   return (
-    <div className="max-w-[400px] min-w-60 border-5 rounded-md border-orange bg-gradient-br-orange rotate-45 relative right-[100px] big-diamond-shadows">
-      <Image src={projectImage} width={500} height={500} alt="" className={`relative right-3 bottom-[31px] desktop:scale-[1.19] laptop:scale-[1.15] -rotate-45`} />
+    <div className="max-w-[360px] min-w-60 border-5 rounded-md border-orange bg-gradient-br-orange rotate-45 relative right-[15%] big-diamond-shadows-small">
+      <Image src={projectImage} width={500} height={500} alt="project image" className={`relative right-3 bottom-[30px] desktop:scale-[1.2] -rotate-45`} />
     </div>
   )
 }
