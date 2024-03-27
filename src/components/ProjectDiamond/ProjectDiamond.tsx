2b99fe1500362e908706cd1caafd9bbb7e09f916
Ricardo Gava <ricardoagava@gmail.com>
2024-03-26 21:16:49 -0300

    chore: adjust pixel size to percentage and adjust the project image to fit better into the diamond

--- src/components/ProjectDiamond/ProjectDiamond.tsx
+++ src/components/ProjectDiamond/ProjectDiamond.tsx
@@ -8,7 +8,7 @@ type ProjectDiamondProps = {
 export const ProjectDiamond = ({ projectImage }: ProjectDiamondProps) => {
   return (
     <div className="max-w-[360px] min-w-60 border-5 rounded-md border-orange bg-gradient-br-orange rotate-45 relative right-[15%] big-diamond-shadows-small">
-      <Image src={projectImage} width={500} height={500} alt="project image" className={`relative right-3 bottom-[30px] desktop:scale-[1.2] -rotate-45`} />
+      <Image src={projectImage} width={500} height={500} alt="project image" className={`relative bottom-[27px] right-[3%] scale-[1.19] -rotate-45`} />
     </div>
   )
 }
