b2d8214babbacf76ce0772cae9bc3891ad78612b
Ricardo Gava <ricardoagava@gmail.com>
2024-03-27 23:49:23 -0300

    style: add a darker opacity for each project tag and a hover opacity to be a little lighter

--- src/components/ProjectThumbnail/ProjectThumbnail.tsx
+++ src/components/ProjectThumbnail/ProjectThumbnail.tsx
@@ -29,7 +29,7 @@ export const ProjectThumbnail = ({ thumbImage, projectIndex }: ProjectThumbnailP
 
   return (
     <li onClick={toogleActive} className="relative w-full modal-lg:h-20 h-16 flex justify-center items-center border-t-[3px] last:border-b-[3px] border-white box-content">
-      <div className={`absolute z-10 w-full modal-lg:h-20 h-16 bg-[rgba(0,0,0,.6)] ${isActive && 'hidden'}`}></div>
+      <div className={`absolute z-10 w-full modal-lg:h-20 h-16 bg-[rgba(0,0,0,.65)] hover:opacity-65 ${isActive && 'hidden'}`}></div>
       <div className={`${isActive && "h-10 w-10 absolute border-b-[3px] border-l-[3px] rounded-sm rotate-45 bg-dark-green -right-[64px] cursor-default"}`}></div>
       <Image src={thumbImage} fill priority alt="project thumbnail" />
     </li>
