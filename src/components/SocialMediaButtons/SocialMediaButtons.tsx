f63213f792350adf897db3e9796da2a10845ed92
Ricardo Gava <ricardoagava@gmail.com>
2024-03-31 02:08:11 -0300

    refactor: SocialMediaButtons component refactored to use composition pattern

--- src/components/SocialMediaButtons/SocialMediaButtons.tsx
+++ src/components/SocialMediaButtons/SocialMediaButtons.tsx
@@ -1,12 +1,11 @@
-import { SocialMediaButton } from "../SocialMediaButton/SocialMediaButton"
+type MediaButtonProps = {
+  children: React.ReactNode
+}
 
-export const SocialMediaButtons = () => {
+export const SocialMediaButtons = ({ children }: MediaButtonProps) => {
   return (
     <div className="absolute tablet:top-[10%] top-[6%] laptop:mr-2 flex desktop:gap-12 gap-10">
-      <SocialMediaButton src='/github.svg' link="https://github.com/RicarGit" alt='github link' />
-      <SocialMediaButton src='/linkedin.svg' link="https://www.linkedin.com/in/ricardo-gava-dev/" alt='linkedin link' />
-      <SocialMediaButton src='/discord.svg' link="https://discord.gg/e5djqg63u3" alt='discord link' />
-      <SocialMediaButton src='/telegram.svg' link="https://t.me/RicardoGava" alt='telegram link' />
+      {children}
     </div>
   )
 }
