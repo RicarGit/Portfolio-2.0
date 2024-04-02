369001f18c49c64626695afb671ff522fe45da17
Ricardo Gava <ricardoagava@gmail.com>
2024-04-02 15:54:40 -0300

    refactor: refactoring the CloseSideMenuButton component, now it uses a svg icon and it was repositioned to be inside the SideMenu

--- src/components/CloseSideMenuButton/CloseSideMenuButton.tsx
+++ src/components/CloseSideMenuButton/CloseSideMenuButton.tsx
@@ -1,15 +1,13 @@
+import Image from "next/image"
 import Link from "next/link"
+
+import close from "/public/close.svg"
 
 export const CloseSideMenuButton = () => {
   return (
-    <button className="absolute -right-[30px] top-1 leading-none bg-dark-green text-white font-medium text-sm rounded-r-md border-y-[3px] border-r-[3px] cursor-pointer overflow-hidden">
-      <Link href='/' className="block max-w-2 pr-4 pl-2 py-2" >
-        F
-        E
-        C
-        H
-        A
-        R
+    <button className="absolute right-[8%] top-[2%]" >
+      <Link href='/' className="block p-4" >
+        <Image src={close} fill alt="close button" />
       </Link>
     </button>
   )
