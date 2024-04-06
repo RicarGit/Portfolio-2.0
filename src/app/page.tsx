1715110f75b3a005e3ac76b4512e7a0f5228ed7b
Ricardo Gava <ricardoagava@gmail.com>
2024-04-06 16:15:02 -0300

    style: adjusted some spacing on small screen sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default function Home({ searchParams }: HomeProps) {
             <Contact contactType="email" contact="ricardoagava@gmail.com" >Email</Contact>
             <Contact contactType="whatsapp" contact="(11) 97328-3296" >Whatsapp</Contact>
 
-            <p className="tablet:absolute mt-20 mb-3 bottom-4 ml-4 text-dark-blue text-center text-xs tracking-wide" >Todos os direitos reservados.</p>
+            <p className="tablet:absolute mt-20 tablet:bottom-2 modal-lg:bottom-5 mb-2 tablet:mb-0 ml-4 text-dark-blue text-center text-xs tracking-wide" >Todos os direitos reservados.</p>
           </Footer>
         </SideMenu>
       }
