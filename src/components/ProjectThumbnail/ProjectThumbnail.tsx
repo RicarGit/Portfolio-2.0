c8cf165298f43e75f8d9c26a1251203eba305254
Ricardo Gava <ricardoagava@gmail.com>
2024-04-06 15:57:12 -0300

    feat: add new sizes for small screens

--- src/components/ProjectThumbnail/ProjectThumbnail.tsx
+++ src/components/ProjectThumbnail/ProjectThumbnail.tsx
@@ -16,7 +16,7 @@ export const ProjectThumbnail = ({ project }: ProjectThumbnailProps) => {
         <Link href={`?project=${index}`} className="relative flex items-center modal-lg:h-20 h-full tablet:h-16" >
           <div className={`absolute z-10 w-full modal-lg:h-20 tablet:h-16 h-full bg-[rgba(0,0,0,.65)] hover:opacity-65 ${project === index.toString() ? 'hidden' : ''}`}></div>
 
-          <div className={`${project === index.toString() ? "hidden tablet:block h-10 w-10 absolute border-b-[3px] border-l-[3px] rounded-sm rotate-45 bg-dark-green -right-[64px] cursor-default" : ''}`}></div>
+          <div className={`${project === index.toString() ? "hidden tablet:block size-[26px] laptop:size-10 absolute border-b-[3px] border-l-[3px] rounded-sm rotate-45 bg-dark-green -right-[43px] laptop:-right-[64px] cursor-default" : ''}`}></div>
 
           <Image src={thumbImage} fill priority alt="project thumbnail" />
         </Link>
