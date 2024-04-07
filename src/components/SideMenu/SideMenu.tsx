683f9b66b255dbcc992b6478b0049e2585af762b
Ricardo Gava <ricardoagava@gmail.com>
2024-04-07 00:26:07 -0300

    style: using border top and bottom on mobile

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -4,7 +4,7 @@ type SideMenuProps = {
 
 export const SideMenu = ({ children }: SideMenuProps) => {
   return (
-    <aside className="absolute modal-lg:w-[230px] w-full tablet:w-[210px] z-30 pt-7 flex flex-col h-dvh bg-dark-green tablet:border-r-[3px] tablet:border-y-[3px] rounded-r bg-[radial-gradient(circle_at_0_25%,_var(--tw-gradient-stops))] from-green via-dark-green via-70% to-dark-green overflow-y-auto tablet:overflow-y-visible">
+    <aside className="absolute modal-lg:w-[230px] w-full tablet:w-[210px] z-30 pt-7 flex flex-col h-dvh bg-dark-green border-r-[3px] border-y-[3px] rounded-r bg-[radial-gradient(circle_at_0_25%,_var(--tw-gradient-stops))] from-green via-dark-green via-70% to-dark-green overflow-y-auto tablet:overflow-y-visible">
       {children}
     </aside>
   )
