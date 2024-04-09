c0375ecb66cdcfc34101d8c99962f0a11d651a24
Ricardo Gava <ricardoagava@gmail.com>
2024-04-09 00:48:48 -0300

    feat: add a border left on mobile, also moved the radial bg a little to the top and decreased a little the radial size

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -4,7 +4,7 @@ type SideMenuProps = {
 
 export const SideMenu = ({ children }: SideMenuProps) => {
   return (
-    <aside className="absolute modal-lg:w-[230px] w-full tablet:w-[210px] z-30 pt-7 flex flex-col h-dvh bg-dark-green border-r-[3px] border-y-[3px] rounded-r bg-[radial-gradient(circle_at_0_25%,_var(--tw-gradient-stops))] from-green via-dark-green via-70% to-dark-green overflow-y-auto tablet:overflow-y-visible">
+    <aside className="absolute modal-lg:w-[230px] w-full tablet:w-[210px] z-30 pt-7 flex flex-col h-dvh bg-dark-green border-x-[3px] tablet:border-l-0 border-y-[3px] rounded-r bg-[radial-gradient(circle_at_0_20%,_var(--tw-gradient-stops))] from-green via-dark-green via-65% to-dark-green overflow-y-auto tablet:overflow-y-visible">
       {children}
     </aside>
   )
