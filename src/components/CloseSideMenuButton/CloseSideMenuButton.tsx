2ba7aa4e65331c0012711e58e03f1719b5ebd25a
Ricardo Gava <ricardoagava@gmail.com>
2024-04-09 00:39:07 -0300

    style: add relative position to the image parent as asked by the Next warning

--- src/components/CloseSideMenuButton/CloseSideMenuButton.tsx
+++ src/components/CloseSideMenuButton/CloseSideMenuButton.tsx
@@ -6,7 +6,7 @@ import close from "/public/close.svg"
 export const CloseSideMenuButton = () => {
   return (
     <button className="absolute right-5 top-5 tablet:right-3 tablet:top-3" >
-      <Link href='/' className="block p-4 tablet:p-3" >
+      <Link href='/' className="relative block p-4 tablet:p-3" >
         <Image src={close} fill alt="close button" />
       </Link>
     </button>
