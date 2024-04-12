3dbee7f359038f8c46cdf79cf4e811b8890fef80
Ricardo Gava <ricardoagava@gmail.com>
2024-04-12 00:19:35 -0300

    feat: add an id for each thumbnail and add a hash followed by the project's name to Next Link scroll the current project to the top

--- src/components/ProjectThumbnail/ProjectThumbnail.tsx
+++ src/components/ProjectThumbnail/ProjectThumbnail.tsx
@@ -15,8 +15,8 @@ export const ProjectThumbnail = ({ project, children }: ProjectThumbnailProps) =
 
   return (
     projectsData.map(({ projectName, thumbImage }, index) => (
-      <li key={projectName} className="relative border-t-[3px] last:border-b-[3px] border-white">
-        <Link href={`${project === index.toString() ? '?menu=true' : `?project=${index}`}`} className="relative flex items-center modal-lg:h-20 h-full tablet:h-16" >
+      <li key={projectName} id={projectName.replaceAll(' ', '')} className="relative border-t-[3px] last:border-b-[3px] border-white">
+        <Link href={`${project === index.toString() ? '?menu=true' : `?project=${index}`}#${projectName.replaceAll(' ', '')}`} className="relative flex items-center modal-lg:h-20 h-full tablet:h-16" >
           <div className={`absolute z-10 w-full modal-lg:h-20 tablet:h-16 h-full bg-[rgba(0,0,0,.65)] laptop:hover:opacity-65 ${project === index.toString() ? 'hidden' : ''}`}></div>
 
           <div className={`${project === index.toString() ? "hidden tablet:block size-[26px] laptop:size-10 absolute border-b-[3px] border-l-[3px] rounded-sm rotate-45 bg-dark-green -right-[43px] laptop:-right-[64px] cursor-default" : ''}`}></div>
