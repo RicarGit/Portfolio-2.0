d217fc4a74caf490d6d886f16f61c57d31f6cbff
Ricardo Gava <ricardoagava@gmail.com>
2024-04-12 00:33:03 -0300

    feat: add an animation to the Button image to be hidden for 0.8 seconds at the start of the animation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -22,7 +22,7 @@ export const Button = ({ color, link, children }: ButtonProps) => {
       <Link href={isExternalLink ? link : `?${link}=true`} target={isExternalLink ? '_blank' : ''} className={`relative desktop:w-36 w-28 desktop:h-10 h-8 ${isOrange ? 'desktop:mr-8 mr-5' : 'desktop:ml-8 ml-5'} flex items-center justify-center animate-widthAnimation`} >
         <div className={`absolute ${isOrange ? 'desktop:right-[-25px] -right-4' : 'desktop:left-[-25px] -left-4'} buttonDiamond animate-rotateRight ${buttonColors[color]}`}></div>
         <span className={`z-10 ${isOrange ? 'text-orange' : 'text-light-green'} desktop:text-sm text-xs font-bold uppercase tracking-widest animate-hidden`}>{children}</span>
-        <Image src={buttonSvg} fill alt="bg-button" className={`${isOrange && 'rotate-180'}`} />
+        <Image src={buttonSvg} fill alt="bg-button" className={`${isOrange && 'rotate-180'} animate-[hidden_0.8s_ease_forwards]`} />
       </Link>
     </button>
   )
