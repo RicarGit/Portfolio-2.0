e6a62c1afb09e942c27801a5a5c193344b6c66a4
Ricardo Gava <ricardoagava@gmail.com>
2024-04-16 22:28:40 -0300

    feat: add a 405deg rotate animation to the sub title diamond

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -39,6 +39,7 @@ const config: Config = {
         widthAnimation: 'widthAnimation 2s ease forwards',
         rotateRight: 'rotateRight 1.5s 0.5s ease forwards',
         rotateLeft: 'rotateLeft 2s ease forwards',
+        rotateRight405: 'rotateRight405 2s ease forwards',
         socialMediaRotate: 'socialMediaRotate 2s ease forwards',
         hidden: 'hidden 2s ease forwards',
         shadows: 'shadows 2s ease forwards',
@@ -59,6 +60,9 @@ const config: Config = {
         rotateLeft: {
           'from': { transform: 'rotate(0deg)' },
           'to': { transform: 'rotate(-45deg)' }
+        },
+        rotateRight405: {
+          'to': { transform: 'rotate(405deg)' }
         },
         socialMediaRotate: {
           '0%': { transform: 'rotate(0)', 'box-shadow': 'none' },
