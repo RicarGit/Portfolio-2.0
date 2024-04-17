1a52748e042e458ca12498a5292fc70df8c381a2
Ricardo Gava <ricardoagava@gmail.com>
2024-04-17 03:22:20 -0300

    refactor: refactoring the gradient animation to use background position on the animation and set the background size in the css utility class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
         socialMediaRotate: 'socialMediaRotate 2s ease forwards',
         hidden: 'hidden 2s ease forwards',
         shadows: 'shadows 2s ease forwards',
-        gradient: 'gradient 3s ease-in-out forwards',
+        gradient: 'gradient 3s ease forwards',
         fadeIn: 'fadeIn 2s ease forwards',
         fadeInTop: 'fadeInTop 2s ease forwards',
         fadeInBottom: 'fadeInBottom 2s ease forwards'
@@ -86,8 +86,8 @@ const config: Config = {
           '70%': { 'box-shadow': 'none', filter: 'drop-shadow(0 0 #000)' }
         },
         gradient: {
-          '0%': { 'background-size': '100% 100%' },
-          '100%': { 'background-size': '100% 600%' }
+          '0%': { backgroundPosition: '100% 100%' },
+          '100%': { backgroundPosition: '100% 0%' }
         },
         fadeIn: {
           '0%': { transform: 'translateX(-20%)', opacity: '0' },
