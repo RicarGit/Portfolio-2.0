ad71444cb435761c07edc8c8a7ca8e0e00ebf644
Ricardo Gava <ricardoagava@gmail.com>
2024-04-22 00:20:40 -0300

    feat: add a gap of 8px for small screen sizes and add style prop to the images with a width auto to get rid of Next warning width resize because of the size animation

--- src/components/Knowledges/Knowledges.tsx
+++ src/components/Knowledges/Knowledges.tsx
@@ -6,7 +6,7 @@ export const Knowledges = () => {
     <>
       <h3 className='desktop:text-lg text-center tablet:text-left text-orange desktop:mt-8 mt-6 animate-hidden'>Conhecimentos: </h3>
 
-      <div className='flex w-80 desktop:w-[420px] desktop:gap-3 mt-3 desktop:h-10 h-8 overflow-x-hidden animate-widthAnimation'>
+      <div className='flex w-80 desktop:w-[420px] gap-2 desktop:gap-3 mt-3 desktop:h-10 h-8 overflow-x-hidden animate-widthAnimation'>
         {learnedTechs.length > 0 &&
           learnedTechs.map(({ image, alt }, index) => (
             <Image
@@ -14,6 +14,7 @@ export const Knowledges = () => {
               src={image}
               width={40}
               height={40}
+              style={{ width: 'auto' }}
               alt={alt}
             />
           ))}
