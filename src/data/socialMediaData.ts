f2a27d7266d6c56387704e2293ae3efcfc684a9b
Ricardo Gava <ricardoagava@gmail.com>
2024-04-22 00:00:01 -0300

    feat: add social media datas

---
+++ src/data/socialMediaData.ts
@@ -0,0 +1,22 @@
+export const socialMediaData = [
+  {
+    image: "/images/svg/github.svg",
+    link: "https://github.com/RicarGit",
+    alt: "github link"
+  },
+  {
+    image: "/images/svg/linkedin.svg",
+    link: "https://www.linkedin.com/in/ricardo-gava-dev/",
+    alt: "linkedin link"
+  },
+  {
+    image: "/images/svg/discord.svg",
+    link: "https://discord.gg/e5djqg63u3",
+    alt: "discord link"
+  },
+  {
+    image: "/images/svg/telegram.svg",
+    link: "https://t.me/RicardoGava",
+    alt: "telegram link"
+  }
+]
