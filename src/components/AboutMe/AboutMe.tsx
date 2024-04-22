550fff239ec477c355382294c6546846aef1b6c8
Ricardo Gava <ricardoagava@gmail.com>
2024-04-21 23:46:58 -0300

    feat: AboutMe component now has a section tag

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -2,11 +2,12 @@ import { Knowledges } from "../Knowledges/Knowledges"
 
 export const AboutMe = () => {
   return (
-    <>
+    <section>
       <p className="mt-4 text-center tablet:text-left desktop:w-[500px] laptop:w-[430px] w-full desktop:mr-[111px] laptop:pr-6 desktop:pr-0 text-light-green laptop:tracking-wide desktop:tracking-wider animate-fadeIn">
         Especializado em Front-End com foco na criação de interfaces dinâmicas e responsivas, estou comprometido em criar soluções web de alta qualidade, busco constantemente aprimorar a experiência do usuário através de soluções eficientes e modernas. Explore meu portfólio para ver meu trabalho em ação!
       </p>
 
       <Knowledges />
+    </section>
   )
 }
