dca3fc9ecd717c23db9cbc3d919dccfe4e795720
Ricardo Gava <ricardoagava@gmail.com>
2024-04-28 23:47:47 -0300

    refactor: Footer component do not receive children props anymore, instead receive the footer content from Home

--- src/components/Menu/Footer/Footer.tsx
+++ src/components/Menu/Footer/Footer.tsx
@@ -1,11 +1,21 @@
-type FooterProps = {
-  children: React.ReactNode
-}
+import { Contact, SubTitle } from "@/components"
 
-export const Footer = ({ children }: FooterProps) => {
+export const Footer = () => {
   return (
     <footer className="h-full relative">
-      {children}
+      <SubTitle>Contato</SubTitle>
+
+      <Contact contactType="email" contact="ricardoagava@gmail.com" >
+        Email
+      </Contact>
+
+      <Contact contactType="whatsapp" contact="(11) 97328-3296" >
+        Whatsapp
+      </Contact>
+
+      <p className="tablet:absolute mt-20 tablet:bottom-2 modal-lg:bottom-5 mb-2 tablet:mb-0 ml-4 text-dark-blue text-center text-xs tracking-wide" >
+        2024®Todos os direitos reservados.
+      </p>
     </footer>
   )
 }
